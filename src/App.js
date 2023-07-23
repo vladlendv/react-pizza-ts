245c4f82b29560c095c8f7a0ea5689ad2d415794
Vladlen01~ <tubebiznes@gmail.com>
2023-07-23 21:38:06 +0500

    add skeleton for pizza blocks

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Sort from "./components/Sort"
 import PizzaBlock from "./components/PizzaBlock"
 import { initialState } from "./state"
 import { useEffect, useState } from "react"
+import "react-loading-skeleton/dist/skeleton.css"
+import PizzaLoader from "./components/PizzaLoader"
 
 const App = () => {
   const { categories } = initialState
   const [pizzaList, setPizzaList] = useState([])
   const [orderQuantity, setOrderQuantity] = useState(0)
-  
+
   useEffect(() => {
     fetch("https://64ba3cb25e0670a501d5d86e.mockapi.io/pizza")
-      .then(data => data.json())
-      .then(res => setPizzaList(res))
-  })
+      .then((data) => data.json())
+      .then((res) => setPizzaList(res))
+  }, [])
 
   return (
     <div className="wrapper">
@@ -27,17 +29,19 @@ const App = () => {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzaList.map((item) => (
-              <PizzaBlock
-                setOrderQuantity={setOrderQuantity}
-                key={item.id}
-                title={item.title}
-                types={item.types}
-                sizes={item.sizes}
-                price={item.price}
-                imageUrl={item.imageUrl}
-              />
-            ))}
+            {!pizzaList.length
+              ? [...new Array(6)].map((el, index) => <PizzaLoader key={index} />)
+              : pizzaList.map((item) => (
+                  <PizzaBlock
+                    setOrderQuantity={setOrderQuantity}
+                    key={item.id}
+                    title={item.title}
+                    types={item.types}
+                    sizes={item.sizes}
+                    price={item.price}
+                    imageUrl={item.imageUrl}
+                  />
+                ))}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaLoader.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const PizzaLoader = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={292}
+    height={482}
+    viewBox="0 0 292 482"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="144" cy="125" r="118" /> 
+    <rect x="61" y="272" rx="0" ry="0" width="170" height="14" /> 
+    <rect x="0" y="313" rx="0" ry="0" width="292" height="90" /> 
+    <rect x="150" y="423" rx="19" ry="19" width="140" height="40" /> 
+    <rect x="1" y="430" rx="0" ry="0" width="95" height="20" />
+  </ContentLoader>
+)
+
+export default PizzaLoader
